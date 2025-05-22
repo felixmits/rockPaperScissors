188657ff3df73c49aa3173e2ebfca365baf10933
felix <65243200+felixmits@users.noreply.github.com>
2025-05-22 22:58:40 +0300

    that took way too long (it's not gonna go well) added a computer choice

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,9 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  return computerChoice;
+}
+
+console.log(getComputerChoice());

d4eaabffe2d5694c42f89de5f03913891a1e3c42
felix <65243200+felixmits@users.noreply.github.com>
2025-05-25 17:36:56 +0300

    fixed some stuff. not sure what

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -7,10 +7,29 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  const humanChoice = prompt("Choose rock, paper or scissors.");
-  return humanChoice;
+  const humanChoice = prompt("Choose rock, paper or scissors.").toLowerCase();
+  return ("Your choice is " +humanChoice);
 }
 
 const humanScore = 0;
 const computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "scissors") {
+    return ("Rock beats Scissors. You win");
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    return ("Paper beats Rock. You lose");
+  }  else if (humanChoice === "scissors" && computerChoice === "rock" ){
+    return ("Rock beats Scissors. You lose");
+  }  else if (humanChoice === "scissors" && computerChoice === "paper"){
+    return ("Scissors beat Paper. You win");
+  } else if (humanChoice === "paper" && computerChoice === "rock"){
+    return ("Paper beats Rock. You win");
+  } else if (humanChoice === "paper" && computerChoice === "scissors"){
+    return ("Scissors beat Paper. You lose")
+  } else if (humanChoice === computerChoice) {
+    return ("It's a tie!");
+  }
+} 
+
+playRound(getHumanChoice, getComputerChoice);
